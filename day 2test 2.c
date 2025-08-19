c8d43d5fe238baeaab5f359125c220dc10f6c1d5
sayan mondal <smsayanmondal06@gmail.com>
2025-08-19 17:52:51 +0530

    Add files via upload

---
+++ day 2test 2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+// r : radius of the circle
+// a : area of the circle
+int main(){
+    float r;
+    printf("enter the value of r");
+    scanf("%f",&r);
+    printf("area of the circle is : %f",3.14*r*r);
+    return 0;
+}
