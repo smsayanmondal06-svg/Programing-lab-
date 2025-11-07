452f809b2e415a24cb2a0197d7cc39967d744975
sayan <smsayanmondal06@gmail.com>
2025-11-07 23:47:56 +0530

    delete

--- day 1test 1.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    printf("ADAMAS UNIVERSITY");
-    return 0;
-}
--- day 3 test1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int n;
-    printf("enter the value of n");
-    scanf("%d",&n);
-    printf("last digit of the number without modulas is :%d\n",n-n/10*10);
-    printf("last digit of the number with modulas is:%d\n",n%10);
-    return 0;
-}
